ca0b6c6c23d56d5a9fcbb85275bc5e1aece5a7ef
DaniilBesaga <{134208729}+{DaniilBesaga}@users.noreply.github.com>
2024-05-21 13:01:13 +0300

    Add the db context

---
+++ DemiWebsite/DemiWebsite.Server/Data/DatabaseContext.cs
@@ -0,0 +1,35 @@
+﻿using DemiWebsite.Server.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace DemiWebsite.Server.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+        {
+        }
+        public DatabaseContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        { }
+
+        public DbSet<WorkItem> Works { get; set; }
+        public DbSet<NewsItem> News { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<WorkItem>().HasData(
+                new WorkItem()
+                {
+
+                }
+            );
+
+            modelBuilder.Entity<NewsItem>().HasData(
+                new NewsItem()
+                {
+
+                }
+            );
+        }
+    }
+}
