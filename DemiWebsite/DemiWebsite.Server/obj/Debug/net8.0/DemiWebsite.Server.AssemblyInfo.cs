bcd8f0db6cb147e44883d83d9a92d4e876848674
DaniilBesaga <{134208729}+{DaniilBesaga}@users.noreply.github.com>
2024-05-21 12:32:56 +0300

    Add the models

--- DemiWebsite/DemiWebsite.Server/obj/Debug/net8.0/DemiWebsite.Server.AssemblyInfo.cs
+++ DemiWebsite/DemiWebsite.Server/obj/Debug/net8.0/DemiWebsite.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemiWebsite.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbbcc8e34df4dc48aea8ff1c9163334f770f89e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemiWebsite.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemiWebsite.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
